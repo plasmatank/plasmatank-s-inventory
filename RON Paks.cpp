708f37967ff01c250f293c7d8ed483edaeb5b155
Plasmatank <59257419+plasmatank@users.noreply.github.com>
2023-01-16 10:19:32 +0800

    Add files via upload

---
+++ RON Paks.cpp
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+	fstream file; string pre_path,path; smatch mat;
+	file.open(getenv("appdata") + string("/../Local/ReadyOrNot/Saved/Logs/Modio.log"), ios::in);
+	while (getline(file, pre_path))
+	{
+		if (regex_search(pre_path, mat, *(new regex("ReadyOrNot"))))
+		{
+			for (sregex_iterator pos(pre_path.cbegin(), pre_path.cend(), *(new regex("[A-Z]:\.+Paks"))), end; pos != end; pos++)
+			{
+				path = pos->str();
+			}
+		}
+	}
+	file.close();
+	system((string("explorer.exe ") + path).c_str());
+	return 0;
+}
